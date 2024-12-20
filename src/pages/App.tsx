f31bbe1ec987bcf44b05134eea42c03e15fe2bc7
flyingPie23 <drissfreah325@gmail.com>
2024-12-20 22:56:53 +0100

    set up app and assest

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import '../assests/application.scss';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="landing-page">
+        <h1>hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
