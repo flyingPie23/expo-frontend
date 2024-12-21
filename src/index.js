51ec9262fb4fceda8845f3da04e13ca87a412a36
flyingPie23 <drissfreah325@gmail.com>
2024-12-21 20:41:16 +0100

    set up signing form front

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assests/application.scss';
+
 import App from './pages/App.tsx';
+import LoginPage from './pages/LoginPage.tsx';
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,22 @@
 import '../assests/application.scss';
 import React from 'react';
 
+import SignInForm from '../components/SignInForm.tsx';
+
 const inputform = {
-  signup: {lable: "email", class: "input-feild" }
+  signup: [
+    {lable: "email"},
+    {lable: "password" }
+  ]
+
 
 }
 class LoginPage extends React.Component {
   public render() {
+    const {signup} = inputform
     return (
       <div id="login-page">
+        <SignInForm signup={signup} />
       </div>
     );
   }
