7a908414be2b720f703c16a62db84322bf58c708
driss <freah>
2025-01-05 20:36:30 +0100

    worked on the feed page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,14 +3,17 @@ import React from "react";
 
 import { logout } from "../scripts/authHandler.tsx"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faMagnifyingGlass, faBell, faUser, faGear} from '@fortawesome/free-solid-svg-icons'
 
 
 
 
-const SideBar = (props: { brand: {name: string, to: string, class: string}; tabs:Array<{ name: string, to: string, class: string }>; logoff:{name: string, class: string} }) => {
+const SideBar = (props: { brand: {name: string, to: string, class: string}; tabs:Array<{ id: number, name: string, to: string, class: string}>; logoff:{name: string, class: string} }) => {
   const {brand, tabs, logoff} = props
+  const icons = [faHome, faMagnifyingGlass, faBell, faUser, faGear]
   const BrandLink: any = () => <a href={brand.to} className={brand.class}> {brand.name} </a>
-  const TabLink: any = () => tabs.map((tab:{name: string, to: string, class: string}) => <li> <a key={tab.name} href={tab.to} className={tab.class}> {tab.name} </a> </li> )
+  const TabLink: any = () => tabs.map((tab:{id: number, name: string, to: string, class: string}) => <li> <a key={tab.name} href={tab.to} className={tab.class}> <FontAwesomeIcon icon={icons[tab.id]} /> {tab.name} </a> </li> )
   const LogoutLink: any = () => <a onClick={logout} className={logoff.class}> {logoff.name} </a>
   return(
     <div id="side-bar">
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -5,11 +5,11 @@ import Sidebar from "../components/Sidebar.tsx"
 const navigation = {
   brand: {name: "Ex-po", to: "/", class:"home-btn"},
   tabs: [
-    {name: "home", to: "/", class:"tab-btn"},
-    {name: "search", to: "/search", class:"tab-btn"},
-    {name: "notifications", to: "/notifications", class:"tab-btn"},
-    {name: "profile", to: "/profile", class:"tab-btn"},
-    {name: "settings", to: "/settings", class:"tab-btn"},
+    {id: 0, name: "home", to: "/", class:"tab-btn"},
+    {id: 1, name: "search", to: "/search", class:"tab-btn"},
+    {id: 2, name: "notifications", to: "/notifications", class:"tab-btn"},
+    {id: 3, name: "profile", to: "/profile", class:"tab-btn"},
+    {id: 4, name: "settings", to: "/settings", class:"tab-btn"},
   ],
   logoff:{name: "logout", class:"logout-btn"}
 }
@@ -18,8 +18,6 @@ interface Feedtab {
   discover: boolean,
   following: boolean
 }
-
-
 
 class Feed extends Component<{}, Feedtab> {
   state: Feedtab = {
@@ -44,30 +42,34 @@ class Feed extends Component<{}, Feedtab> {
       <div id="feed-page">
         <Sidebar brand={brand} tabs={tabs} logoff={logoff} />
 
-        <div className="feed-header">
-          <h1 onClick={this.active} className={`feed-tab ${this.state.discover ? 'active' : ''}`}>Discover</h1>
-          <h1 onClick={this.active} className={`feed-tab ${this.state.following ? 'active' : ''}`}>Following</h1>
+        <div className='feed-scroll'>
+          <div className="feed-header">
+            <h1 onClick={this.active} className={`feed-tab ${this.state.discover ? 'active' : ''}`}>Discover</h1>
+            <h1 onClick={this.active} className={`feed-tab ${this.state.following ? 'active' : ''}`}>Following</h1>
+          </div>
+
+          <div className="feed-results">
+            <div className="feed-content">
+              {this.state.discover ? (
+                Array.from({ length: 100 }).map((_, index) => (
+                <div key={index}>
+                  <h1>Discover</h1>
+                  <p>feed time</p>
+                </div>
+                ))
+              ) : (
+                Array.from({ length: 100 }).map((_, index) => (
+                <div key={index}>
+                  <h1>Following</h1>
+                  <p>feed time</p>
+                </div>
+                ))
+              )}
+            </div>
+          </div>
+
         </div>
 
-        <div className="feed-results">
-          <div className="feed-content">
-            {this.state.discover ? (
-              Array.from({ length: 100 }).map((_, index) => (
-              <div key={index}>
-                <h1>Discover</h1>
-                <p>feed time</p>
-              </div>
-              ))
-            ) : (
-              Array.from({ length: 100 }).map((_, index) => (
-              <div key={index}>
-                <h1>Following</h1>
-                <p>feed time</p>
-              </div>
-              ))
-            )}
-          </div>
-        </div>
       </div>
     );
   }
